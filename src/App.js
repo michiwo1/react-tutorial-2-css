e725913f4327c4f7704f69bc6270457de6635e99
michiwo <54099086+michiwo1@users.noreply.github.com>
2021-04-15 12:00:31 +0900

    styled jsxの当て方の練習

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { InlineStyle } from "./component/InlineStyle";
+import { CssModules } from "./component/CssModules";
 import "./styles.css";
+import { StyledJsx } from "./component/StyledJsx";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
+      <StyledJsx />
     </div>
   );
 }
