16e3bdfa12ca97fa871e4dc9b2e79b497da06c77
michiwo <54099086+michiwo1@users.noreply.github.com>
2021-04-15 12:33:30 +0900

    Emotionの使い方

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { CssModules } from "./component/CssModules";
 import "./styles.css";
 import { StyledJsx } from "./component/StyledJsx";
 import { StyledComponents } from "./component/StyledComponents";
+import { Emotion } from "./component/Emotion";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <CssModules />
       <StyledJsx />
       <StyledComponents />
+      <Emotion />
     </div>
   );
 }
